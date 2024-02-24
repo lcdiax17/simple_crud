b3bcfa07e80bdadf729c2b68d85991e8a5577243
Lucas Dias <lucasalvesdiasdasilva123@gmail.com>
2024-02-24 13:16:16 -0300

    Simple CRUD complete

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,26 @@ function ReadNames(){
 }
 
 function RemoveNames(id: number){
-    nameList.splice(id, 1)
+    const removeName = nameList.splice(id, 1)
+    return removeName
 }
 
 function UpdateNames(id: number, name: string){
     nameList.splice(id, 1, name)
 }
+
+// AddNames("Alex")
+// AddNames("Otavio")
+// AddNames("Roberto")
+
+// ReadNames()
+
+// RemoveNames(1)
+
+// ReadNames()
+
+// UpdateNames(1, "Osvaldo")
+
+// ReadNames()
+
+/* As chamadas de função acima são referentes aos testes propostos do exercício */
